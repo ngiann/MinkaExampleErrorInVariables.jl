2e3e42b015ea93541844c81bc3b791f86405d5f8
ngiann <ngiann@gmail.com>
2023-07-21 10:27:58 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.38

---
+++ src/MinkaExampleErrorInVariables.jl
@@ -0,0 +1,5 @@
+module MinkaExampleErrorInVariables
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MinkaExampleErrorInVariables
+using Test
+
+@testset "MinkaExampleErrorInVariables.jl" begin
+    # Write your tests here.
+end
